0982388972e9f937f434fa895f9b21ca572335f2
floyare <neptuneeex@gmail.com>
2024-12-02 11:36:30 +0100

    ci: optimized rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
+import terser from '@rollup/plugin-terser';
 
 export default {
     input: 'src/index.ts',
@@ -14,6 +15,11 @@ export default {
             format: 'esm',
         },
     ],
-    plugins: [resolve(), commonjs(), typescript()],
-    external: ['react', 'react-dom'],
+    plugins: [
+        resolve(),
+        commonjs(),
+        typescript(),
+        terser(),
+    ],
+    external: ['react', 'react-dom', 'jotai'],
 };
