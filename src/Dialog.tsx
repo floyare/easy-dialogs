bd113205fd967fc0b20703fd5d8dd52942830ec6
floyare <neptuneeex@gmail.com>
2024-12-14 22:07:43 +0100

    perf: :zap: optimized package
    changed React import in Dialog.tsx to just use type of React

--- src/Dialog.tsx
+++ src/Dialog.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import type React from 'react';
 import { activeDialog } from './atoms';
 import { useAtom } from 'jotai';
 
--- src/atoms.ts
+++ src/atoms.ts
@@ -1,4 +1,2 @@
 import { atom } from "jotai";
-//import { DialogType } from "./types/DialogTypes";
-
 export const activeDialog = atom<any | null>(null)
